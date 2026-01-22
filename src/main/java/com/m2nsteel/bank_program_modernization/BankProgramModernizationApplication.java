67bd3f772ca34545cee0c16e8959290de9a0221b
mincheol <fhdnwlqkd@hanyang.ac.kr>
2026-01-23 00:21:36 +0900

    feat: init banking modernization project

---
+++ src/main/java/com/m2nsteel/bank_program_modernization/BankProgramModernizationApplication.java
@@ -0,0 +1,13 @@
+package com.m2nsteel.bank_program_modernization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProgramModernizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankProgramModernizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2nsteel/bank_program_modernization/BankProgramModernizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2nsteel.bank_program_modernization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankProgramModernizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

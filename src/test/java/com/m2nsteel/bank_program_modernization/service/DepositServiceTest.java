67c155e46fb4abdeb4a9041ed0ab2c366d8a886f
mincheol <fhdnwlqkd@hanyang.ac.kr>
2026-01-25 21:02:12 +0900

    refactor: DepositServiceTest setUp 로직 생성

--- src/test/java/com/m2nsteel/bank_program_modernization/service/DepositServiceTest.java
+++ src/test/java/com/m2nsteel/bank_program_modernization/service/DepositServiceTest.java
@@ -5,11 +5,14 @@ import com.m2nsteel.bank_program_modernization.dto.request.AccountCreateRequest;
 import com.m2nsteel.bank_program_modernization.dto.request.DepositRequest;
 import com.m2nsteel.bank_program_modernization.dto.request.MemberSignUpRequest;
 import com.m2nsteel.bank_program_modernization.dto.response.AccountResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -19,24 +22,28 @@ class DepositServiceTest {
     @Autowired AccountService accountService;
     @Autowired MemberService memberService;
 
+    private String accountNum;
+    private final String accountPassword = "password1234";
+
+    @BeforeEach
+    void setUp() {
+        // 1.가입 및 계좌 생성
+        var member = memberService.signUp(new MemberSignUpRequest("member1", "p1", "M1", "Member", 1L));
+        var account = accountService.createAccount(new AccountCreateRequest(member.memberId(), 1L, accountPassword));
+        accountNum = account.accountNumber();
+    }
+
     @Test
-    @DisplayName("입금 성공 테스트")
+    @DisplayName("입금 성공 테스트: 잔액 확인")
     void deposit() {
-        // 1. Given: 테스트를 위한 준비 (회원 가입, 계좌 생성)
-        MemberSignUpRequest signUpRequest = new MemberSignUpRequest(
-                "testuser", "password123", "M123456", "John Doe", 1L
-        );
-        var memberResponse = memberService.signUp(signUpRequest);
-        Long branchId = 1L;
-        AccountCreateRequest accountRequest = new AccountCreateRequest(memberResponse.memberId(), branchId, "1234");
-        AccountResponse accountResponse = accountService.createAccount(accountRequest);
+        // 1. Given: 테스트를 위한 준비
         String requestId = "unique-request-id-123";
         long depositAmount = 5000L;
 
         // 2. When: 입금 요청 수행
         DepositRequest depositRequest = new DepositRequest(
                 requestId,
-                accountResponse.accountNumber(),
+                accountNum,
                 depositAmount
         );
         var response = transactionService.deposit(depositRequest);
@@ -47,21 +54,14 @@ class DepositServiceTest {
     }
 
     @Test
-    @DisplayName("중복 요청 테스트")
+    @DisplayName("입금 실패: 중복 요청 테스트")
     void duplicateDeposit() {
         // 1. Given: 테스트를 위한 준비 (회원 가입, 계좌 생성, 첫 입금)
-        MemberSignUpRequest signUpRequest = new MemberSignUpRequest(
-                "testuser", "password123", "M123456", "John Doe", 1L
-        );
-        var memberResponse = memberService.signUp(signUpRequest);
-        Long branchId = 1L;
-        AccountCreateRequest accountRequest = new AccountCreateRequest(memberResponse.memberId(), branchId, "1234");
-        AccountResponse accountResponse = accountService.createAccount(accountRequest);
         String requestId = "unique-request-id-123";
         long depositAmount = 5000L;
         DepositRequest depositRequest = new DepositRequest(
                 requestId,
-                accountResponse.accountNumber(),
+                accountNum,
                 depositAmount
         );
         var transactionResponse = transactionService.deposit(depositRequest);
@@ -73,22 +73,15 @@ class DepositServiceTest {
     }
 
     @Test
-    @DisplayName("잘못된 입금액 테스트")
+    @DisplayName("입금 실패: 잘못된 입금액 테스트")
     void invalidDepositAmount() {
-        MemberSignUpRequest signUpRequest = new MemberSignUpRequest(
-                "testuser", "password123", "M123456", "John Doe", 1L
-        );
-        var memberResponse = memberService.signUp(signUpRequest);
-        Long branchId = 1L;
-        AccountCreateRequest accountRequest = new AccountCreateRequest(memberResponse.memberId(), branchId, "1234");
-        AccountResponse accountResponse = accountService.createAccount(accountRequest);
         String requestId = "unique-request-id-123";
         long depositAmount = -5000L;
 
         // 2. When: 입금 요청 수행
         DepositRequest depositRequest = new DepositRequest(
                 requestId,
-                accountResponse.accountNumber(),
+                accountNum,
                 depositAmount
         );
         assertThrows(BusinessException.class, () -> {
